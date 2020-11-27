0b702263c3d5575a57af75d2951911cb0f796601
Tomás Castro <tcastrog10@gmail.com>
2020-11-27 08:36:34 -0300

    eslint + prettier reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+
+import logo from './logo.svg'
+
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,13 +16,12 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
